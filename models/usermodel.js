ed193305a7787b5a400d3424fe9478483c2dccb8
wieartha783 <wayanwidya.artana@mitrais.com>
2022-02-10 09:24:31 +0800

    Add Model and Migration

---
+++ models/usermodel.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    email_verified_at: DataTypes.DATE,
+    password: DataTypes.STRING,
+    remember_token: DataTypes.STRING,
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
